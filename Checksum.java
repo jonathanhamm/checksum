b958adbf229c7b134ced89f8aec7c44d56515d32
Jonathan Hamm <jonathan-hamm@utulsa.edu>
2014-03-19 16:31:52 -0500

    Worked some of my original code into the specified class format better.

--- Checksum.java
+++ Checksum.java
@@ -10,96 +10,133 @@ import java.io.EOFException;
 import java.io.IOException;
 
 public class Checksum
-
 {
-
 /*
-
- Frame format, where n is the number of bytes in the frame:
-
- ___________________________________________________________
-
- byte indices | Description
-
- 0-1 | Header: size of frame in bytes (2 bytes)
-
- 2-13 | Header: reserved for future purpose
-
- 14-(n-3) | Data: datagram
-
- (n-2)-(n-1) | Trailer: checksum value (trailer = 2 bytes)
-
- Header consists of 14 bytes.
-
- Trailer consists of 2 bytes. (the checksum)
-
- Datagram consists of m bytes.
-
- The size of the frame is inFrameSize = n = 14 + 2 + m
-
+	Frame format, where n is the number of bytes in the frame:
+	___________________________________________________________
+	byte indices | Description
+	0-1 | Header: size of frame in bytes (2 bytes)
+	2-13 | Header: reserved for future purpose
+	14-(n-3) | Data: datagram
+	(n-2)-(n-1) | Trailer: checksum value (trailer = 2 bytes)
+	Header consists of 14 bytes.
+	Trailer consists of 2 bytes. (the checksum)
+	Datagram consists of m bytes.
+	The size of the frame is inFrameSize = n = 14 + 2 + m
 */
 
- private byte [] inFrame; // shallow copy of the input frame.
+	private byte [] inFrame; // shallow copy of the input frame.
+	private int inFrameSize; // total bytes in inFrame includes header + trailer
 
- private int inFrameSize; // total bytes in inFrame includes header + trailer
+	public Checksum(byte[] frame, int size)
+	{	 
+		// Make a SHALLOW copy of the input frame.
+		this.inFrame = frame;
+		// Store the frame size in the first two bytes of the input frame. (Assuming big endian)
+		frame[0] = (byte)(size >> 8);
+		frame[1] = (byte)(size);
+	}
 
- public Checksum( byte [] frame, int size )
+	public int getFrameSize()
+	{
+		// return the frame size = n = 14 + 2 + m bytes of the datagram.
+		return this.inFrame.length;
+	}
 
- {
+	public void setFrame(byte[] frame)
+	{
+		// make a SHALLOW copy of the input frame.
+		this.inFrame = frame;
+	}
 
- // Make a SHALLOW copy of the input frame.
+	public void insertChecksum(int chksm)
+	{
+		//Insert the checksum value into the last two bytes of the input frame.
 
- // Store the frame size in the first two bytes of the input frame.
+	}
 
- }
+	public int checksum1()
+	{
+		//calculate and return the first checksum
 
- public int getFrameSize()
+		int sum = 0, wrap;
+		boolean done = false;
+		DataInputStream data = makeStream(this.inFrame);
 
- {
+		while(!done) {
+			try {
+				sum += data.readUnsignedShort();
+			}
+			catch(EOFException eof) {
+				done = true;
+				wrap = (sum & 0xffff0000) >> 16;
+				sum += wrap;
+				try {
+					data.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				continue;
+			}
+			catch(IOException io) {
+				System.err.print("IO Error");
+			}
+		}
+		return (short)~sum;
+	}
 
- // return the frame size = n = 14 + 2 + m bytes of the datagram.
+	public int checksum2()
+	{
+		//calculate and return the second checksum
+		int f = 0, g = 0;
+		boolean done = false;
+		short word = 0;
+		DataInputStream data = makeStream(this.inFrame);
 
- }
+		while(!done) {
+			try {
+				f = (f + data.readUnsignedByte()) % 255;
+				g = (g + data.readUnsignedByte()) % 255;
+			}
+			catch(EOFException eof) {
+				done = true;
+				word = (short)((g << 8) | f);
+				try {
+					data.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				continue;
+			}
+			catch(IOException io) {
+				System.err.print("IO Error");
+			}
+		}
+		return (short)~word;
 
- public void setFrame( byte [] frame )
+	}
 
- {
+	public int checksum3()
+	{
+		// calculate and return the third checksum
+		return 0;
+	}
 
- // make a SHALLOW copy of the input frame.
+	private static DataInputStream makeStream(byte[] data)
+	{
+		if((data.length % 2) != 0) {
+			int i;
+			byte[] data2 = new byte[data.length+1];
 
- }
+			for(i = 0; i < data.length; i++)
+				data2[i] = data[i];
+			data2[i] = 0;
+			data = data2;
+		}
 
- public void insertChecksum( int chksm )
+		return new DataInputStream(new ByteArrayInputStream(data));
+	}
 
- {
-
- // Insert the checksum value into the last two bytes of the input frame.
-
- }
-
- public int checksum1()
-
- {
-
- // calculate and return the first checksum
-
- }
-
- public int checksum2()
-
- {
-
- // calculate and return the second checksum
-
- }
-
- public int checksum3()
-
- {
-
- // calculate and return the third checksum
-
- }
 
 }
 
@@ -110,46 +147,46 @@ public class Checksum {
 
 	private byte[] inFrame;
 	private int inFrameSize;
-		
+
 	/*	private byte[] frame;
-	
+
 	public Checksum() 
 	{
 		this.frame = null;
 	}
-	
+
 	public Checksum(byte[] frame)
 	{
 		this.frame = frame;
 	}
-	
-	
+
+
 	public int getFrameSize() 
 	{
 		return 0;
 	}
-	
+
 	private static DataInputStream makeStream(byte[] data)
 	{
 		if((data.length % 2) != 0) {
 			int i;
 			byte[] data2 = new byte[data.length+1];
-			
+
 			for(i = 0; i < data.length; i++)
 				data2[i] = data[i];
 			data2[i] = 0;
 			data = data2;
 		}
-			
+
 		return new DataInputStream(new ByteArrayInputStream(data));
 	}
-	
+
 	public static short checksum1(byte[] frame) 
 	{
 		int sum = 0, wrap;
 		boolean done = false;
 		DataInputStream data = makeStream(frame);
-		
+
 		while(!done) {
 			try {
 				sum += data.readUnsignedShort();
@@ -176,7 +213,7 @@ public class Checksum {
 		boolean done = false;
 		short word = 0;
 		DataInputStream data = makeStream(frame);
-		
+
 		while(!done) {
 			try {
 				f = (f + data.readUnsignedByte()) % 255;
